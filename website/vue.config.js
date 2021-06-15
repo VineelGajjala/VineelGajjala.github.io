01d98ccf7098d2fe9177c3702e379d139a37f199
vineelgajjala <gajjala.vineel@gmail.com>
2021-06-14 23:24:59 -0500

    initial commit

--- website/vue.config.js
+++ website/vue.config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+  
   publicPath: process.env.NODE_ENV === 'production'
   ? '/VineelGajjala.github.io/'
   : '/'
-  transpileDependencies: [
-    'vuetify'
-  ]
 }
